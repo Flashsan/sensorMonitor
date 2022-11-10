76677bf80c3a7df6ddf93ea0eb68811db6e9b18d
Aliaksandr Hryharovich <algrigr@gmail.com>
2022-11-10 22:11:25 +0300

    I've started to perform a test task "Sensor monitor".

---
+++ src/main/java/com/labinvent/sensorMonitor/SensorMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.labinvent.sensorMonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensorMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SensorMonitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/labinvent/sensorMonitor/SensorMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.labinvent.sensorMonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensorMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
